e48af551ef5a6bf99d7e1a99e14d8e0053b820d3
Asmin Shrestha <asmin.stha77@gmail.com>
2025-10-17 22:02:21 +0545

    Day 20 : Django To-Do App – Add, Edit, Mark, and Delete Tasks

--- D19/ToDo/todo/urls.py
+++ D19/ToDo/todo/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 from . import views 
 urlpatterns =[
-path('addTask/' , views.addTask, name='addTask')
+    #add task
+path('addTask/' , views.addTask, name='addTask'),
+# mark as done 
+path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+# mark as not done
+path('mark_as_not_done/<int:pk>/', views.mark_as_not_done, name='mark_as_not_done'),
+
+#edit features
+path('edit_task/<int:pk>/' , views.edit_task, name='edit_task'),
+
+# delete task
+path('delete_task/<int:pk>/' , views.delete_task, name='delete_task'),
 
 ]
